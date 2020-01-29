e4eef82b2ae1d4811293886b352d2875c4c75a76
PanikosTheodosiou <pt9301@my.acm.ac.uk>
2020-01-29 14:59:09 +0000

    init
    First commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed = 1;
+    public float JumpForce = 75;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Speed*Time.deltaTime,0,0);  
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(-Speed * Time.deltaTime, 0, 0);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * JumpForce);
+        }
+
+
+
+
+
+
+    }
+}
