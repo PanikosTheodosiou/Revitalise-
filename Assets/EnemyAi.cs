52a5ebf2c55a3abd7115d1971125df11e364ac11
Chikaalanrobinson <car9519@my.acm.ac.uk>
2020-02-12 16:26:17 +0000

    Applied everything

--- Assets/EnemyAi.cs
+++ Assets/EnemyAi.cs
@@ -4,47 +4,15 @@ using UnityEngine;
 
 public class EnemyAi : MonoBehaviour
 {
-    public float speed;
-    public bool movingRight = true;
-    public Transform groundDetection;
-    public float SwitchDistance = 2;
-
-    public GameObject WayPointA;
-    public GameObject WayPointB;
-
-
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        Debug.Log(Vector2.Distance(this.transform.position, WayPointA.transform.position));
-
-        if (movingRight == true)
-        {
-            transform.Translate(Vector2.right * speed * Time.deltaTime);
-
-            if (Vector2.Distance(this.transform.position, WayPointA.transform.position) <= SwitchDistance)
-            {
-                movingRight = false;
-            }
-        }
-        else
-        {
-            transform.Translate(Vector2.right * -speed * Time.deltaTime);
-
-            if (Vector2.Distance(this.transform.position, WayPointB.transform.position) <= SwitchDistance)
-            {
-                movingRight = true;
-            }
-        }
-
-      
+        
     }
 }
-
