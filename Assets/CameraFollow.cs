0e50c0ee55cb86d5796ed53a5c1def042dbab27f
PanikosTheodosiou <pt9301@my.acm.ac.uk>
2020-02-20 15:06:33 +0000

    Fixed Camera 20_02_2020

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float offset;
+
+    void Start()
+    {
+        offset = (this.transform.position - target.position).z;
+    }
+
+
+    void Update()
+    {
+        this.transform.position = target.transform.position + new Vector3(0, 0, offset);
+    }
+}
