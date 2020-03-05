989bfafd58103e574280ce78acfcace5f5652ac8
PanikosTheodosiou <pt9301@my.acm.ac.uk>
2020-03-05 17:06:34 +0000

    Created a New Project

---
+++ Audio/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * Time.deltaTime * speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        Debug.Log("collision");
+
+
+
+        if (collision.gameObject.CompareTag("Rob"))
+        {
+            Debug.Log("My name is Rob");
+        }
+        else if(collision.gameObject.CompareTag ("Enemy"))
+        {
+            Debug.Log("I am the Enemy");
+        }
+    }
+}
