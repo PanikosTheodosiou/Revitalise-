036637c9e07a0bfc0fc8a59efe3b79916680c423
PanikosTheodosiou <pt9301@my.acm.ac.uk>
2020-04-21 21:35:25 +0100

    plts

---
+++ Assets/Scripts/EnemyAi.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAi : MonoBehaviour
+{
+    public float speed;
+    public bool movingRight = true;
+    public float SwitchDistance = 2;
+
+    public int currentWaypoint;
+    public List<GameObject> Waypoints = new List<GameObject>();
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Vector2.Distance(this.transform.position, Waypoints[currentWaypoint].transform.position) >= SwitchDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Waypoints[currentWaypoint].transform.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            currentWaypoint+=1;
+
+            if (currentWaypoint > Waypoints.Count - 1)
+                currentWaypoint = 0;
+        }
+
+
+        //if (movingRight == true)
+        //{
+
+        //    transform.position = Vector2.MoveTowards(transform.position, WayPointA.transform.position, speed * Time.deltaTime);
+
+        //    if (Vector2.Distance(this.transform.position, WayPointA.transform.position) <= SwitchDistance)
+        //    {
+        //        movingRight = false;
+        //    }
+        //}
+        //else
+        //{
+        //    transform.position = Vector2.MoveTowards(transform.position, WayPointB.transform.position, speed * Time.deltaTime);
+
+        //    if (Vector2.Distance(this.transform.position, WayPointB.transform.position) <= SwitchDistance)
+        //    {
+        //        movingRight = true;
+        //    }
+        //}
+
+
+    }
+}
+
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int hp;
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp < 1)
+        {
+            Kill();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Projectile")
+        {
+            hp--;
+        }
+    }
+
+    void Kill()
+    {
+        Destroy(this.gameObject);
+    }
+
+}
+   
+
+
+
