2714d7ab691f790b604dc77fd195916496d8e422
PanikosTheodosiou <pt9301@my.acm.ac.uk>
2020-06-09 23:47:37 +0100

    Pick Ups

---
+++ Assets/PickUp.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.gameObject.tag == "Pick Up")
+        {
+            Destroy(target.gameObject);
+
+        }
+
+        else if (target.gameObject.tag == "Pick Up 2")
+        {
+            Destroy(target.gameObject);
+        }
+
+        else if (target.gameObject.tag == "Pick Up 3")
+        {
+            Destroy(target.gameObject);
+        }
+
+
+
+
+    }
+}
