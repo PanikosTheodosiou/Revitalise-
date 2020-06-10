b47808ddc85bd96debbcd01aa2082b99fed283dc
PanikosTheodosiou <pt9301@my.acm.ac.uk>
2020-06-10 05:18:34 +0100

    hp bars

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     public int hp;
     public GameObject bullet;
+    public Image healthBar;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +18,13 @@ public class Health : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
+
         if (hp < 1)
         {
             Kill();
         }
+        healthBar.fillAmount = hp / 100f;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
