d64609f0891f0878bd99c8353af938ab8a3f69f5
PanikosTheodosiou <pt9301@my.acm.ac.uk>
2020-06-15 04:49:23 +0100

    canvas

---
+++ Assets/END.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+public class END : MonoBehaviour
+{
+    public Canvas endCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        endCanvas = GetComponent<Canvas>();
+    }
+
+     void OnTriggerEnter2D(Collider2D collision)
+    {
+        endCanvas.enabled = true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
